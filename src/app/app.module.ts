da87ba31899617b6da1eb975bc8410a53662df9b
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-09 15:39:08 -0500

    Staring to build the addition component...
    Remembered to import the mat controls I plan to use...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,8 @@ import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatFormFieldModule
   ],
   providers: [
     provideAnimationsAsync()
