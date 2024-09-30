208098dacb5eeeddbaa6ec3bb5db1184cb8474f7
dcharmon <dcorbinharmon@gmail.com>
2024-09-30 10:46:33 -0500

    Generated a pizza toppings component.
    Here is the command we used to generate the new component...
    
    ng generate component pizza-toppings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaToppingsComponent
   ],
   imports: [
     BrowserModule,
