3fcb36d615ef81f347493aabaa2ace606ba37b1c
tsteelematc <tsteele@madisoncollege.edu>
2024-10-01 11:36:50 -0500

    Setting up for the jsf-online-week-06-09-pre vid...
    So that I can show the changes, and commit them ! ! !

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,50 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PizzaService } from '../pizza.service';
-
-interface PizzaToppingDisplay {
-  name: string;
-  price: number;
-  checked: boolean;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrls: ['./pizza-toppings.component.css']
 })
-export class PizzaToppingsComponent implements OnInit {
+export class PizzaToppingsComponent {
 
-  // Magic DI... Dependency injection...
-  constructor(
-    private pizzaSvc: PizzaService
-  ) { }
-
-  availablePizzaToppings: PizzaToppingDisplay[] = [];
-
-  ngOnInit(): void {
-
-    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
-    console.log(pt);
-
-    this.availablePizzaToppings = pt.map(
-      x => ({
-        ...x
-        , checked: false
-      })
-    );
-
-    console.log(this.availablePizzaToppings);
-  }
-
-  totalPrice = 0;
-
-  calculateTotal = () => {
-    this.totalPrice = this.availablePizzaToppings
-      .filter(x => x.checked)
-      .reduce(
-        (acc, x) => acc + x.price
-        , 0
-      )
-    ;
-  }
 }
