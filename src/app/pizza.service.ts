6f04b4af0cd51d10c27af75a7c376c3c1602d07a
melsummy <132179052+melsummy@users.noreply.github.com>
2024-10-02 12:53:29 -0500

    Boom ! ! ! We're calling a DI'd service!
    This is fun, next, lets show the pizza toppings in the component..

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingFromTheCloud() {
+
+    // pretend we make a web service call
+  const toppingsFromWebService = [
+    {
+      name: 'Peperoni',
+      price: 3.25
+    },
+    {
+      name: 'oregano',
+      price: 1.10
+    },
+    {
+      name: 'Olive',
+      price: 2.25
+    }
+  ];
+  return toppingsFromWebService;
+  }
 }
