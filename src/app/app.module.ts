61d59b6ed001637859da36cf865cf341d627ad64
Darin <dwellons@madisoncollege.edu>
2024-10-02 21:54:12 -0500

    We need bi directional binding for checkboxes
    I am learning this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    PizzaToppingsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PizzaToppingsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
