a15cf07ad9e62d321932175d7051eca3d3300269
Darin <dwellons@madisoncollege.edu>
2024-10-02 21:49:49 -0500

    Add a checked property - map
    Using map to add a checked property to the pizza toppings

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -10,10 +16,24 @@ export class PizzaToppingsComponent implements OnInit {
   // Magic DI (Dependency Injection)
   constructor(private pizzaSvc: PizzaService) {}
 
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
   // gets called when component is created and ready to initialize it
   ngOnInit(): void {
     const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
 
+    // logging out pizza toppings
     console.log(pt);
+
+    // mapping
+    this.availablePizzaToppings = pt.map((x) => ({
+      // spread in existing x properties
+      ...x,
+      // adding in checked property
+      checked: false,
+    }));
+
+    // loading out toppings with added checked property
+    console.log(this.availablePizzaToppings);
   }
 }
