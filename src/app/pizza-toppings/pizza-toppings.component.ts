d1f108e6a910abf5719ce61963ba45bbb172c50a
BRivera03 <brivera@edgewood.edu>
2024-10-04 14:17:19 -0500

    Were calling a DI'd service

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrl: './pizza-toppings.component.css'
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit{
+
+  //Magic DI = dependency injection
+  constructor (
+    private pizzaSvc: PizzaService
+  ) { }
+
+  ngOnInit(): void{
+
+    const pt = this.pizzaSvc.getPizzaToppingsFromtheCloud();
+    console.log(pt);
+  }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromtheCloud() {
+    //pretend we make a web service call from here.
+    const toppingsFromWebService = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+      , {
+        name: 'Pepperoni'
+        , price: 1.50
+      }
+      ,
+      {
+        name: 'Sausage'
+        , price: 1.75
+      }
+    ];
+
+    return toppingsFromWebService;
+  }
 }
