631dd1779c971c95f2fd199e07861ee569719d70
BRivera03 <brivera@edgewood.edu>
2024-10-06 13:58:33 -0500

    Setup to use material design components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.componen
 
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +21,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     AppRoutingModule
     , FormsModule
+    , BrowserAnimationsModule
+    , MatButtonModule
+    ,  MatCardModule
+    , MatCheckboxModule
   ],
   providers: [
     provideAnimationsAsync()
