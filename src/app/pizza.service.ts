0cd92980f085b123f088e6283685d66f44187327
BRivera03 <brivera@edgewood.edu>
2024-10-06 21:44:31 -0500

    Modified image component to take image sources
    Began styling card for image component

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -13,14 +13,18 @@ export class PizzaService {
       {
         name: 'Olives'
         , price: 2.25
+        , image: 'src/assets/img/pizzaOlives.png'
       }
       , {
         name: 'Pepperoni'
         , price: 1.50
+        , image: 'src/assets/img/pepperoniPizza.avif'
       }
       , {
         name: 'Sausage'
         , price: 1.75
+        , image: 'src/assets/img/sausage.Pizza.avif'
+
       }
     ];
 
--- src/app/toggle-image/toggle-image.component.ts
+++ src/app/toggle-image/toggle-image.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
+
+interface ToppingsImage{
+  name: string;
+  price: number;
+  image: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-toggle-image',
   templateUrl: './toggle-image.component.html',
   styleUrl: './toggle-image.component.css'
 })
-export class ToggleImageComponent {
+export class ToggleImageComponent implements OnInit{
+  constructor(
+    private pizzaSvc: PizzaService
+  ) { }
+
+  ToppingsImage: ToppingsImage[] = [];
+
+  ngOnInit(): void {
+
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
+    console.log(pt);
+
+    this.ToppingsImage = pt.map(
+      x => ({
+        ...x
+        , checked: false
+      })
+    );
+
+    console.log(this.ToppingsImage);
+  }
 
 }
