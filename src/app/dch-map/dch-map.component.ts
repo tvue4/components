34b722732f68bd978f2e3e24a2aa76072dec7592
dcharmon <dcorbinharmon@gmail.com>
2024-10-07 19:36:34 -0500

    Added Map
    Uses openstreetmap
    
    AfterViewInit called after view is fully initialized, better for map

--- src/app/dch-map/dch-map.component.ts
+++ src/app/dch-map/dch-map.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-dch-map',
   templateUrl: './dch-map.component.html',
-  styleUrl: './dch-map.component.css'
+  styleUrls: ['./dch-map.component.css']
 })
-export class DchMapComponent {
+export class DchMapComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = L.map('map').setView([43.1210, -89.3307], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 15
+    }).addTo(map);
+  }
 
 }
