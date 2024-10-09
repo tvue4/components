5e58546f257c8d3e925e01529c0a728204199090
Darin <dwellons@madisoncollege.edu>
2024-10-08 19:52:50 -0500

    Automatically calculating the total
    changes to automatically calculate the total

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -10,41 +10,29 @@ interface PizzaToppingDisplay {
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
-  styleUrls: ['./pizza-toppings.component.css']
+  styleUrls: ['./pizza-toppings.component.css'],
 })
 export class PizzaToppingsComponent implements OnInit {
-
   // Magic DI... Dependency injection...
-  constructor(
-    private pizzaSvc: PizzaService
-  ) { }
+  constructor(private pizzaSvc: PizzaService) {}
 
   availablePizzaToppings: PizzaToppingDisplay[] = [];
 
   ngOnInit(): void {
-
     const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
     console.log(pt);
 
-    this.availablePizzaToppings = pt.map(
-      x => ({
-        ...x
-        , checked: false
-      })
-    );
+    this.availablePizzaToppings = pt.map((x) => ({
+      ...x,
+      checked: false,
+    }));
 
     console.log(this.availablePizzaToppings);
   }
 
-  totalPrice = 0;
-
-  calculateTotal = () => {
-    this.totalPrice = this.availablePizzaToppings
-      .filter(x => x.checked)
-      .reduce(
-        (acc, x) => acc + x.price
-        , 0
-      )
-    ;
+  get totalPrice() {
+    return this.availablePizzaToppings
+      .filter((x) => x.checked)
+      .reduce((acc, x) => acc + x.price, 0);
   }
 }
