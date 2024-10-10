f67c5bfbf297c5d44e44a31060528702cb1ac167
fisevine <sevans10@madisoncollege.edu>
2024-10-10 10:48:46 -0500

    Starting to build the addition component.
    Remembered to import the mat controls!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,23 +12,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SfeAdditionComponent } from './sfe-addition/sfe-addition.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-    , PizzaToppingsComponent, SfeAdditionComponent
-  ],
+  declarations: [AppComponent, PizzaToppingsComponent, SfeAdditionComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    , FormsModule
-    , MatCardModule
-    , MatButtonModule
-    , MatCheckboxModule
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
