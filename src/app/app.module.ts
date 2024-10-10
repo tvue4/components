7a9eb88c1ac955fb16272bbb0b90a774c08f0059
Darin <dwellons@madisoncollege.edu>
2024-10-09 19:04:11 -0500

    generated my addition component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DmwAdditionComponent } from './dmw-addition/dmw-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    , PizzaToppingsComponent
+    , PizzaToppingsComponent, DmwAdditionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dmw-addition/dmw-addition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dmw-addition',
+  templateUrl: './dmw-addition.component.html',
+  styleUrl: './dmw-addition.component.css'
+})
+export class DmwAdditionComponent {
+
+}
