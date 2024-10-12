a030dbf1021f55bd95569d56d75fb70f61a8a88d
Jemal-G <jgeda@madisoncollege.edu>
2024-10-11 21:31:20 -0500

    adding the multiplication of twoo number
    adding necessary html and other codes for products of number

--- src/app/num-products/num-products.component.ts
+++ src/app/num-products/num-products.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './num-products.component.css'
 })
 export class NumProductsComponent {
+  firstnumber = 2;
+  secondnumber = 3;
+  result = 6;
+  multiply=() => this.result = this.firstnumber * this.secondnumber;
 
 }
