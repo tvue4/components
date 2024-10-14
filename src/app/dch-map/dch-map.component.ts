2fe31ca21b35e9f70fecd1220e89e87a6c7223ac
dcharmon <dcorbinharmon@gmail.com>
2024-10-14 17:44:27 -0500

    Added interface, async/await

--- src/app/dch-map/dch-map.component.ts
+++ src/app/dch-map/dch-map.component.ts
@@ -1,11 +1,25 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
+interface OverpassElements {
+  lat: number;
+  lon: number;
+  tags: {
+    name: string;
+    [key: string]: string;
+  };
+}
+
+interface OverpassResponse {
+  elements: OverpassElements[];
+}
+
 @Component({
   selector: 'app-dch-map',
   templateUrl: './dch-map.component.html',
   styleUrls: ['./dch-map.component.css']
 })
+
 export class DchMapComponent implements AfterViewInit {
 
   map!: L.Map;
@@ -21,37 +35,37 @@ export class DchMapComponent implements AfterViewInit {
     this.fetchPizzaPlaces();
   }
 
-  fetchPizzaPlaces(): void {
+  async fetchPizzaPlaces(): Promise<void> {
+    
     const overpassQuery = `
       [out:json][bbox:43.0376,-89.5535,43.1497,-89.2703];
-      node
-        ["cuisine"="pizza"];
+      node["cuisine"="pizza"];
       out;
     `;
-
+  
     const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(overpassQuery)}`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        data.elements.forEach((place: any) => {
-          const lat = place.lat;
-          const lon = place.lon;
-          const name = place.tags.name; 
-
-          let pizzaIcon = L.icon({
-            iconUrl: 'pizzaIcon.png',
-            iconSize: [40, 40],
-            iconAnchor: [20, 40],
-            popupAnchor: [0, 20],
-        });
-
-
-          L.marker([lat, lon], {icon: pizzaIcon})
-            .addTo(this.map)
-            .bindPopup(`<b>${name}</b>`);
-        });
-      })
-      .catch(error => console.error('Error fetching pizza places:', error));
+  
+    try {
+      const response = await fetch(url);
+      const data: OverpassResponse = await response.json();
+  
+      const pizzaIcon = L.icon({
+        iconUrl: 'pizzaIcon.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -20],
+      });
+  
+      data.elements.forEach(({ lat, lon, tags }) => {
+        const name = tags.name;
+  
+        L.marker([lat, lon], { icon: pizzaIcon })
+          .addTo(this.map)
+          .bindPopup(`<b>${name}</b>`);
+      });
+  
+    } catch (error) {
+      console.error('Error fetching pizza places:', error);
+      }
   }
 }
