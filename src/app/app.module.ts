d79578f8984fde567410e7ad6096e4e9ddfbb70e
melsummy <132179052+melsummy@users.noreply.github.com>
2024-10-17 09:39:31 -0500

    i think i fixed it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,23 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ToggleImageComponent } from './toggle-image/toggle-image.component';
+import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
+import { SfeAdditionComponent } from './sfe-addition/sfe-addition.component';
+import { DchMapComponent } from './dch-map/dch-map.component';
 import { PizzaCalculatorComponent } from './pizza-calculator/pizza-calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaToppingsComponent,
+    AppComponent,
+    PizzaToppingsComponent,
+    PizzaCalculatorComponent,
+    ToggleImageComponent,
+    TjsMultiplicationComponent,
+    SfeAdditionComponent,
+    DchMapComponent,
     PizzaCalculatorComponent,
   ],
   imports: [
