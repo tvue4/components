fa43bafe762de7697d98bdc906571b5069c377c4
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-10-22 09:43:54 -0500

    Pizza crust is a mat-select!
    Converted pizza crust selector to be "dynamic" instead of "static" - i.e. more crust types can be added and the Crust Selector will automatically show new types.

--- src/app/ndh-pizza-selector/ndh-pizza-selector.component.ts
+++ src/app/ndh-pizza-selector/ndh-pizza-selector.component.ts
@@ -6,6 +6,12 @@ interface PizzaSize {
   checked: boolean;
 }
 
+interface PizzaCrust {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-ndh-pizza-selector',
   templateUrl: './ndh-pizza-selector.component.html',
@@ -13,7 +19,7 @@ interface PizzaSize {
 })
 export class NdhPizzaSelectorComponent {
 
-  pizzaSizes: PizzaSize[] = [
+  pizzaCrusts: PizzaCrust[] = [
     { name: 'Thin', price: 5.00, checked: false }
     , { name: 'Regular', price: 5.00, checked: false }
     , { name: 'Stuffed', price: 5.00, checked: false }
