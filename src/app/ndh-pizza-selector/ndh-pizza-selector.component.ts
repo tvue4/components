5ba36c33faee8b50f443274caa108a95b6681419
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-10-22 09:52:16 -0500

    Added price display
    And have partial Total Price calculation

--- src/app/ndh-pizza-selector/ndh-pizza-selector.component.ts
+++ src/app/ndh-pizza-selector/ndh-pizza-selector.component.ts
@@ -24,4 +24,26 @@ export class NdhPizzaSelectorComponent {
     , { name: 'Regular', price: 5.00, checked: false }
     , { name: 'Stuffed', price: 5.00, checked: false }
   ];
+
+  get totalPrice() {
+    let sizePrice = this.pizzaCrusts
+    .filter(
+      x => x.checked
+    )
+    .reduce(
+      (acc, x) => acc + x.price
+      , 0
+    )
+  ;
+    let crustPrice = this.pizzaCrusts
+      .filter(
+        x => x.checked
+      )
+      .reduce(
+        (acc, x) => acc + x.price
+        , 0
+      )
+    ;
+      return sizePrice + crustPrice;
+  }
 }
