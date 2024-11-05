8d175f0174f719f966fb008f3116de8749d96e6e
papadawda <dknjai1@madisoncollege.edu>
2024-11-04 22:21:08 -0600

    Created pick up and delivery options
    Added mat-card layout with buttons for selecting service options
    
    Implemented select option method to handle user selection

--- src/app/fee-delivery/fee-delivery.component.ts
+++ src/app/fee-delivery/fee-delivery.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './fee-delivery.component.css'
 })
 export class FeeDeliveryComponent {
+  selectedOption: string | null = null;
+  deliveryAddress: string = '';
+  confirmationMessage: string = '';
 
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.confirmationMessage = '';
+    if (option === 'Pick Up') {
+      this.deliveryAddress = '';
+    }
+  }
+
+  
 }
