5fe9bdfe9a1bdfa6cf3b725f42d5d457033a8665
Deven-K <143856596+Deven-K@users.noreply.github.com>
2024-11-30 20:27:54 -0600

    Create and implement viewmodel interface
    created PizzaToppingDisplay and mapped over api data to create.

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -12,8 +18,19 @@ export class PizzaToppingsComponent implements OnInit {
   // Magic Dependancy Injection
   constructor(private pizzaSvc: PizzaService) {}
 
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
   ngOnInit(): void {
     const toppings = this.pizzaSvc.getPizzaToppingsFromTheCloud()
     console.log(toppings)
+    
+    this.availablePizzaToppings = toppings.map(
+      toppingDisplay => ({
+        ...toppingDisplay,
+        checked: false
+      })
+    );
+
+    console.log(this.availablePizzaToppings)
   }
 }
