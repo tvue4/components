b49a3f383f7882e2ad59d3fc99c8c73219cbf463
Deven-K <143856596+Deven-K@users.noreply.github.com>
2024-11-30 20:16:32 -0600

    Add getPizzaToppings method

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    // pretend to make web service call here
+    const toppingsFromWebService = [
+      {
+        name: 'Olives',
+        price: 2.25
+      },
+      {
+        name: 'Pepperoni',
+        price: 1.50
+      },
+      {
+        name: 'Sausage',
+        price: 1.75
+      }
+    ];
+
+    return toppingsFromWebService;
+  }
 }
