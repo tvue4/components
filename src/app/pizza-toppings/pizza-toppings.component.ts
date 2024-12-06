4b74b8bd3100ec1ab3f8119ad34a62215d0768e8
tvue4 <143903098+tvue4@users.noreply.github.com>
2024-12-06 03:04:35 -0600

    Added PizzaToppingsDisplay Interface...
    And transformed the data to be the proper shape...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -14,11 +20,22 @@ export class PizzaToppingsComponent implements OnInit{
     private pizzaSvc: PizzaService
   ) {}
 
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
   ngOnInit(): void {
     
 
-    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
     console.log(pt);
+
+    this.availablePizzaToppings = pt.map(
+      x => ({
+        ...x 
+        , checked: false
+      })
+    );
+
+    console.log(this.availablePizzaToppings)
   }
 
 }
