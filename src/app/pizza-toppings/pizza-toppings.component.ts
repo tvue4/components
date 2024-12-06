0d54bd93913cdca23c82d01fcbd7c36f095dc522
tvue4 <143903098+tvue4@users.noreply.github.com>
2024-12-06 01:17:33 -0600

    Prepping for next vid...
    ng generate component no longer adds these items

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrl: './pizza-toppings.component.css'
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit{
+
+  constructor () {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
