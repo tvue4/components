53d39cbc6051545dbcefbed264b6b4aaa5f3ba67
tvue4 <143903098+tvue4@users.noreply.github.com>
2024-12-05 19:04:33 -0600

    Generated a pizza toppings component.
    Here's the command used to generate the new component.
    
    ng generate component pizza-toppings

---
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-toppings',
+  templateUrl: './pizza-toppings.component.html',
+  styleUrl: './pizza-toppings.component.css'
+})
+export class PizzaToppingsComponent {
+
+}
