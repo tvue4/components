a771b25e322d5b8393aa7c7a970fa7fe6e10cfbf
tvue4 <143903098+tvue4@users.noreply.github.com>
2024-12-06 18:09:35 -0600

    Setup to use the material design components...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.componen
 
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +21,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     AppRoutingModule
     , FormsModule
+    , MatCardModule
+    , MatButtonModule
+    , MatCheckboxModule
   ],
   providers: [
     provideAnimationsAsync()
