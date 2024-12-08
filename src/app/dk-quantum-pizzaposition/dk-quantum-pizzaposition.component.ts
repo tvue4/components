7366e76bc54feb64c1bea0129583e4fa45e21f15
Deven-K <143856596+Deven-K@users.noreply.github.com>
2024-12-08 09:45:43 -0600

    Implement quantum pizza position component

--- src/app/dk-quantum-pizzaposition/dk-quantum-pizzaposition.component.ts
+++ src/app/dk-quantum-pizzaposition/dk-quantum-pizzaposition.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './dk-quantum-pizzaposition.component.css'
 })
 export class DkQuantumPizzapositionComponent {
+    pizzaMessage: string = "Undetermined."
 
+    determinePizzaFate = () => {
+      const pizzaMessages = [
+        "Your pizza was destroyed in a cosmic incident.",
+        "Your pizza will be delivered safely.",
+        "Your pizza is stuck in rush hour traffic.",
+        "Your pizza was eaten by a grue.",
+        "Your pizza is already in the fridge.",
+        "Your pizza has been eaten by me. Sorry."
+      ]
+
+      this.pizzaMessage = pizzaMessages[Math.floor(Math.random() * pizzaMessages.length)]
+    }
 }
